ecab3a8fd91da20dd91fa2efeb1b6df88a11d33a
Gary Polhill <gary.polhill@hutton.ac.uk>
2021-05-04 15:48:04 +0100

    Added some commandline options

--- search-JASSS.pl
+++ search-JASSS.pl
@@ -30,7 +30,10 @@ use Time::Piece;
 my $index_url = "http://jasss.soc.surrey.ac.uk/index_by_issue.html";
                 # Update this if the JASSS website is changed. It is
                 # the webpage to search for URLs of JASSS articles.
-my $n_context = 50;
+my $n_context = 75;
+my $include_forum = 0;
+my $include_reviews = 0;
+my $include_research = 1;
 
 # Process command-line arguments
 
@@ -38,8 +41,98 @@ if(scalar(@ARGV) == 0) {
   die "Usage: $0 <summary.md> <search terms...>";
 }
 
+while($ARGV[0] =~ /^-/) {
+  my $option = shift(@ARGV);
+
+  if($option eq "-f" || $option eq "--include-forum") {
+    $include_forum = 1;
+  }
+  elsif($option eq "-r" || $option eq "--include-reviews") {
+    $include_reviews = 1;
+  }
+  elsif($option eq "-a" || $option eq "--include-all") {
+    $include_forum = 1;
+    $include_reviews = 1;
+  }
+  elsif($option eq "-F" || $option eq "--only-forum") {
+    $include_forum = 1;
+    $include_reviews = 0;
+    $include_research = 0;
+  }
+  elsif($option eq "-R" || $option eq "--only-reviews") {
+    $include_reviews = 1;
+    $include_forum = 0;
+    $include_research = 0;
+  }
+  elsif($option eq "-c" || $option eq "--context") {
+    $n_context = shift(@ARGV);
+  }
+  elsif($option eq "-i" || $option eq "--index") {
+    $index_url = shift(@ARGV);
+  }
+  elsif($option eq "-h" || $option eq "--help") {
+    print <<USAGE_END;
+$0 {options...} <summary file> <search terms...>
+
+    Search for research articles in JASSS at $index_url
+    and then download and search those articles for the search terms.
+
+    summary file: markdown format file giving detailed search results
+    search terms...: one or more (case insensitive) search terms
+
+   options...:
+\t-a (--include-all): search review, forum and research articles
+\t-c (--context): number of characters to display around each occurrence
+\t-f (--include-forum): include forum articles in the search
+\t-F (--only-forum): only search forum articles
+\t-h (--help): display this message
+\t-r (--include-reviews): include review articles in the search
+\t-R (--only-reviews): only search review articles
+USAGE_END
+  }
+  else {
+    die "Option $option not recognized\n";
+  }
+}
+
 my $summary_file = shift(@ARGV);
 my @search_terms = @ARGV;
+
+if($include_research + $include_reviews + $include_forum == 0) {
+  die "The options you've selected to search for articles mean none will be searched.\n"
+}
+
+my $searched_text;
+
+if($include_research) {
+  if($include_reviews) {
+    if($include_forum) {
+      $searched_text = "research articles, reviews and forum articles";
+    }
+    else {
+      $searched_text = "research articles and reviews";
+    }
+  }
+  else {
+    if($include_forum) {
+      $searched_text = "research and forum articles";
+    }
+    else {
+      $searched_text = "research articles";
+    }
+  }
+}
+elsif($include_reviews) {
+  if($include_forum) {
+    $searched_text = "reviews and forum articles";
+  }
+  else {
+    $searched_text = "reviews";
+  }
+}
+else {
+  $searched_text = "forum articles";
+}
 
 # Extract a list of JASSS articles
 
@@ -54,23 +147,47 @@ if(!$index->is_html()) {
   die "Content type of JASSS index URL \"$index_url\" is not HTML\n";
 }
 
-my @articles;
+my @research;
+my @reviews;
+my @forum;
 my $n_index_links = 0;
 foreach my $link ($index->links()) {
   $n_index_links++;
   if($link->url() =~ /\/(\d+)\/(\d+)\/(\d+)\.html?$/i) {
     my ($vol, $iss, $art) = ($1, $2, $3);
-    push(@articles, [$link->url(), $vol, $iss, $art, $link->text()]);
+    push(@research, [$link->url(), $vol, $iss, $art, $link->text()]);
+  }
+  elsif($link->url() =~ /\/(\d+)\/(\d+)\/review(\d+)\.html?$/i
+    || $link->url() =~ /\/(\d+)\/(\d+)\/reviews\/(\w+)\.html?$/i) {
+
+    my ($vol, $iss, $review) = ($1, $2, $3);
+    push(@reviews, [$link->url(), $vol, $iss, $review, $link->text()]);
+  }
+  elsif($link->url() =~ /\/(\d+)\/(\d+)\/forum\/(\d+)\.html?$/i) {
+    my ($vol, $iss, $art) = ($1, $2, $3);
+    push(@forum, [$link->url(), $vol, $iss, $art, $link->text()]);
   }
 }
 
-if(scalar(@articles) == 0) {
+my @articles = ();
+if($include_research) {
+  @articles = (@articles, @research);
+}
+if($include_reviews) {
+  @articles = (@articles, @reviews);
+}
+if($include_forum) {
+  @articles = (@articles, @forum);
+}
+
+if(scalar(@research) + scalar(@reviews) + scalar(@forum) == 0) {
   die "Unable to extract URLs of any articles from JASSS index URL \"",
     $index_url, "\" ($n_index_links links found)\n";
 }
 else {
-  print "Found ", scalar(@articles), " articles from JASSS index URL \"",
-    $index_url, "\"\n";
+  print "Found ", scalar(@research), " research articles, ", scalar(@reviews),
+    " reviews, and ", scalar(@forum), " forum articles from JASSS index URL \"",
+    $index_url, "\". Searching ", scalar(@articles), ".\n";
 }
 
 # Now search each of the articles for the term
@@ -122,15 +239,20 @@ open(FP, ">", $summary_file)
   or die "Cannot create summary markdown file $summary_file: $!\n";
 
 print FP "# JASSS article search results\n";
-print FP "Search of $n_searched articles of all ", scalar(@articles), " JASSS ",
-  "articles listed on the [JASSS article index page]($index_url) as at ",
-  "$search_time. Search is for whole words (case insensitive).\n";
+print FP "Search (using [search-JASSS.pl](https://github.com/garypolhill/search-JASSS)) ",
+  "of $n_searched articles out of all the ", scalar(@articles)," JASSS ",
+  "$searched_text listed on the [JASSS article index page]($index_url) as at ",
+  "$search_time. Search is for whole words (case insensitive), and articles are ",
+  "listed in descending order of the number of times the search term appears.\n";
 foreach my $search_term (sort @search_terms) {
 
   if($counts{$search_term} > 0) {
     print FP "## Search term `$search_term`\n";
 
-    foreach my $article (sort keys(%{$sentences{$search_term}})) {
+    foreach my $article (sort {
+        scalar(@{$sentences{$search_term}->{$b}}) <=> scalar(@{$sentences{$search_term}->{$a}})
+      } keys(%{$sentences{$search_term}})) {
+
       print FP "### Article [$article]($article)\n";
 
       foreach my $sentence (@{$sentences{$search_term}->{$article}}) {
